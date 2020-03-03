0daf6bc8a0b781be6e73193cef1ba6f919230f90
retiarus <retiarus@gmail.com>
2020-03-02 23:13:45 -0300

    add code to test for cuda

--- main.py
+++ main.py
@@ -59,6 +59,20 @@ def main():
         import torch
         from data_loader import SequenceLoader
 
+        # Setting device on GPU if available, else CPU
+        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        print('Using device:', device)
+        print()
+
+        # Additional Info when using cuda
+        if device.type == 'cuda':
+            print(torch.cuda.get_device_name(0))
+            print('Memory Usage:')
+            print('Allocated:',
+                  round(torch.cuda.memory_allocated(0) / 1024**3, 1), 'GB')
+            print('Cached:   ',
+                  round(torch.cuda.memory_cached(0) / 1024**3, 1), 'GB')
+
         # CUDA
         if args.cuda:
             torch.backends.cudnn.benchmark = True
