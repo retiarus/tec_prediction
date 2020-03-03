3ebbbc5a1d5e48eabcfdc91ab0611c90ede23f75
retiarus <retiarus@gmail.com>
2020-03-02 21:58:13 -0300

    rename data_loader

--- data_loader.py
+++ data_loader.py
@@ -1,95 +1,64 @@
-"""
-Data loader
-
-License is from https://github.com/aboulch/tec_prediction
-"""
-
-import torch.utils.data as data
-
-# import skimage.io
-import numpy as np
-from PIL import Image, ImageMath
 import os
-import os.path
-import h5py
-from datetime import date, timedelta
-import logging
-import random
+import pickle
 
-def make_dataset_train(seq_length):
-    start_train = date(2014,1,1)
-    end_train = date(2016,5,31)
-    days = []
-    day = start_train
-    # break if no possibility to get the next sequence
-    while (day < end_train - timedelta(days=(seq_length) // 12)):
-        for k in range(12): # number of images per day
-            days.append([day,k])
-        day += timedelta(days=1)
-    return days
+import numpy as np
 
-def make_dataset_val(seq_length):
-    start_val = date(2016,7,1)
-    end_val = date(2016,12,31)
-    days = []
-    day = start_val
-    # break if no possibility to get the next sequence
-    while (day < end_val - timedelta(days=(seq_length) // 12)):
-        for k in range(12): # number of images per day
-            days.append([day,k])
-        day += timedelta(days=1)
-    return days
+import pandas as pd
+import torch.utils.data as data
 
 
 class SequenceLoader(data.Dataset):
     """Main Class for Image Folder loader."""
-
-    def __init__(self, root_dir, seqLength, training=True):
+    def __init__(self,
+                 name,
+                 path_files,
+                 seq_length_min,
+                 step_min,
+                 window_train,
+                 window_predict,
+                 data=None):
         """Init function."""
-        #
-        # get the lists of images
 
-        self.TEC_MAP_SHAPE = (72,72)
-        self.seqLength = seqLength
-        self.root_dir = root_dir
-        if training:
-            self.days = make_dataset_train(seqLength)
+        self.name = name
+        self.path_files = path_files
+        self.seq_length_min = seq_length_min
+        self.step_min = step_min
+        self.window_train = window_train
+        self.window_predict = window_predict
+        self.shape = (72, 72)
+        self.data = data
+
+        if data not in ['tec', 'scin', 'tec+scin']:
+            raise Exception(f'No valid data')
+
+        # try load samples from file, if not generate samples from database
+        file = f'samples_{self.name}_{self.seq_length_min}_{self.step_min}_{data}.pkl'
+        path = os.path.join('./data', file)
+        if os.path.isfile(path):
+            self.samples = pickle.load(open(path, "rb"))
+            self.df = pd.read_pickle(
+                os.path.join('./data', f'df_{name}_{data}.pkl'))
         else:
-            self.days = make_dataset_val(seqLength)
-        self.training = training
-
+            raise Exception(f'No valid df and samples list find in {file}')
 
     def load(self, index):
-        day_start, hour_start = self.days[index]
+        index_start, index_end = self.samples[index]
 
-        day = day_start
-        k = hour_start
-        pos = 0
-        
-        dat = np.zeros((self.seqLength, 1,self.TEC_MAP_SHAPE[0], self.TEC_MAP_SHAPE[1]))
+        df_aux = self.df[index_start:index_end]
 
-        for pos in range(self.seqLength):
-            if k>=12:
-                day += timedelta(days=1)
-                k=0
-            year = day.year
-            day_of_year = day.timetuple().tm_yday
-            try:
-                tecdata = np.load(os.path.join(self.root_dir, "tecdata_{0:04d}_{1:03d}.npy".format(year, day_of_year)))
-            except:
-                if self.training:
-                    return self.load(random.randint(0, self.__len__()-1))
-                else:
-                    print("Error validation set")
-            dat[pos,0,:,:] = np.array(Image.fromarray(tecdata[k % 12,:,:]).resize((self.TEC_MAP_SHAPE[1], self.TEC_MAP_SHAPE[0])))
-            k+=1
+        sample = np.zeros((int(self.seq_length_min / self.step_min), 1,
+                           self.shape[0], self.shape[1]))
+        for idx, aux in enumerate(df_aux.itertuples()):
+            sample[idx, 0, :, :] = np.load(
+                os.path.join(self.path_files,
+                             os.path.basename(aux.path).replace('txt', 'npy')))
 
-        return dat, np.array([index])
+        return sample, np.array([index])
 
     def __getitem__(self, index):
         """Get item."""
         return self.load(index)
-        
+
     def __len__(self):
         """Length."""
-        return len(self.days)
+        return len(self.samples)
