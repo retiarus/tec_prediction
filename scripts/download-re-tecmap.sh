52c792055d84093cc436ca67da34dc12b4943885
retiarus <retiarus@gmail.com>
2020-05-13 23:12:55 -0300

    add wget code

--- scripts/download-re-tecmap.sh
+++ scripts/download-re-tecmap.sh
@@ -4,3 +4,5 @@ fileid="17POAs_3-Nssyb1sZdEIcRlf5_g4O7x3F"
 filename="re-tecmap.tar.xz"
 curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}" > /dev/null
 curl -Lb ./cookie "https://drive.google.com/uc?export=download&confirm=`awk '/download/ {print $NF}' ./cookie`&id=${fileid}" -o ${filename}
+
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=${fileid}' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=${fileid}" -O ${filename} && rm -rf /tmp/cookies.txt
